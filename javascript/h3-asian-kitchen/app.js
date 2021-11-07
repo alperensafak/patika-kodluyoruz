50f78aff68addb336ae6c5d3031896d64bb843a8
Alperen <alperensafak@gmail.com>
2021-11-07 23:33:19 +0300

    homework-3

---
+++ javascript/h3-asian-kitchen/app.js
@@ -0,0 +1,69 @@
+
+const section = document.querySelector(".section-center");
+const buttonContainer = document.querySelector(".btn-container");
+
+const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["All"]
+);
+
+const categoryList = () => {
+  const categoryButton = categories
+      .map((category) => {
+        return `<button class="btn btn-outline-dark btn-item" data-id=${category}>${category}</button>`;
+      })
+      .join("");
+
+  buttonContainer.innerHTML = categoryButton;
+  const filterButtons = document.querySelectorAll(".btn-item");
+
+
+  filterButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      console.log(category);
+      const menuCategory = menu.filter((menuItem) => {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "All") {
+        menuList(menu);
+      } else {
+        menuList(menuCategory);
+      }
+    });
+  });
+};
+
+const menuList = (menuItems) => {
+  let displayMenu = menuItems.map((item) => {
+    return `<div class="menu-items col-lg-6 col-sm-12">
+            <img
+              src=${item.img}
+              alt=${item.title}
+              class="photo"
+            />
+            <div class="menu-info">
+              <div class="menu-title">
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </div>
+              <div class="menu-text">
+                ${item.desc}
+              </div>
+            </div>
+          </div>
+    `;
+  });
+  displayMenu = displayMenu.join("");
+  section.innerHTML = displayMenu;
+};
+
+menuList(menu);
+categoryList();
